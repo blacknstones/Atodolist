c1be68fcd813cb093309a37ece14ffe725393bfc
blacknstones <blacknstones@gmail.com>
2020-10-23 17:56:31 +0200

    added javadoc

--- src/main/java/sdaproject/FileManager.java
+++ src/main/java/sdaproject/FileManager.java
@@ -3,9 +3,24 @@ package sdaproject;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * This class is part of the "Atodolist" application.
+ * "Atodolist" is a text-based to-do-list tracker
+ * that runs on the command line.
+ *
+ * This class writes the taskList into a file, and read it when the program restarts.
+ *
+ * @author Yanwei Zhu
+ * @version 2020.10.23
+ */
 public class FileManager {
+
     private static String filename = "todolist.txt";
 
+    /**
+     * Write the taskList to a file to store it.
+     * @param list The current taskList.
+     */
     public void writeToFile(ArrayList<Task> list) {
         try {
             FileOutputStream file = new FileOutputStream(filename);
@@ -19,6 +34,10 @@ public class FileManager {
         }
     }
 
+    /**
+     * Read from file when program restarts.
+     * @return The taskList stored in the file.
+     */
     public ArrayList<Task> readFromFile() {
         ArrayList<Task> list = new ArrayList<>();
         try {
